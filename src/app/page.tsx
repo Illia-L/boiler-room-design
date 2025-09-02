6eda225aa814cc97086840488d9fc0c8ed65df79
Illia Liashko <2275040@gmail.com>
2025-09-02 19:49:42 +0300

    add sections to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import css from './page.module.css';
 
 export default function Home() {
-  return <h1>Головна</h1>;
+  return (
+    <div>
+      <h1>Головна</h1>
+
+      <section>Hero</section>
+
+      <section>Що входить до проєкту</section>
+
+      <section>Наші роботи (слайдер)</section>
+
+      <section>20 років досвіду</section>
+    </div>
+  );
 }
